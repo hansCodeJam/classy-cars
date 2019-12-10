922f0e95c311978426fc938c5f41bfc9472e11d3
Hans Vos <hans.vos@codeimmersives.com>
2019-12-10 15:46:44 -0500

    done except accerlerate

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 class Vehicle {
+  constructor(name, wheelCount, maxSpeed) {
+    this.name = name;
+    this.wheelCount = wheelCount;
+    this.maxSpeed = maxSpeed;
+    this.speed = 0;
+  }
+
+  accerlerate() {
+    this.speed = this.speed + (maxSpeed/3)
+  }
+
+
 }
 
-class Car {
+class Car extends Vehicle {
+  constructor(name) {
+    super(name, 4, 130);
+  }
+  
+  drive() {
+    if(this.maxSpeed < 180) {
+      return "Vroom. "
+    } else {
+
+      return "Vroom. I'm a Ferrari!"
+    }
+  }
 }
 
-class Ferrari {
+class Ferrari extends Car {
+  constructor(name) {
+    super('Ferrari', 4) 
+    this.maxSpeed = 180;
+    
+
+  }
+  
 }
 
 
